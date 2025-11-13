3f6f93bb38a4b8f3fecd905ec3b88dfd62630bee
T-EmmanuelLevinne <emmanuel.tecson@jmc.edu.ph>
2025-11-13 11:25:14 +0800

    Update Appetizer.java

--- wait/MenuItem/Appetizer.java
+++ wait/MenuItem/Appetizer.java
@@ -1,16 +1,11 @@
-// ---------- Menu Item Classes ----------
-
-class Appetizer extends MenuItem {
-
-    public Appetizer(String name, double price, String description) { super(name, price, description); }
-
-    @Override public String getCategory() { return "Appetizer"; }
-
-    @Override public void displayItem() {
-
-        System.out.println("[Appetizer] " + getName() + " - ₱" + getPrice() + " - " + getDescription());
-
+// ---------- Appetizer sublcass of MenuItem ----------
+public class Appetizer extends MenuItem {
+    public Appetizer(String name, double price, String description) {
+        super(name, price, description, "Appetizer");
     }
 
+    @Override
+    public void displayItem() {
+        System.out.println("[Appetizer] " + getName() + " - ₱" + getPrice() + " - " + getDescription());
+    }
 }
-
