f3ec4026bc6b3cfb733dba7b96ab1a9eb5092252
T-EmmanuelLevinne <emmanuel.tecson@jmc.edu.ph>
2025-11-13 11:25:31 +0800

    Update Dessert.java

--- wait/MenuItem/Dessert.java
+++ wait/MenuItem/Dessert.java
@@ -1,15 +1,11 @@
-// ---------- Menu Item Classes ----------
-
-class Dessert extends MenuItem {
-
-    public Dessert(String name, double price, String description) { super(name, price, description); }
-
-    @Override public String getCategory() { return "Dessert"; }
-
-    @Override public void displayItem() {
-
-        System.out.println("[Dessert] " + getName() + " - ₱" + getPrice() + " - " + getDescription());
-
+// ---------- Dessert subclass for menuItem ----------
+public class Dessert extends MenuItem {
+    public Dessert(String name, double price, String description) {
+        super(name, price, description, "Dessert");
     }
 
+    @Override
+    public void displayItem() {
+        System.out.println("[Dessert] " + getName() + " - ₱" + getPrice() + " - " + getDescription());
+    }
 }
